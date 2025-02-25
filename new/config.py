b34d3c39d10159d6c3941b95353bfb64eff43e17
YashasKumar <yashaskumar08@gmail.com>
2025-02-25 14:27:31 +0530

    vercel version changes

--- new/config.py
+++ new/config.py
@@ -1,13 +1,15 @@
 import os
 
 class Config:
-    SECRET_KEY = 'Hello'
-    SQLALCHEMY_DATABASE_URI = f"sqlite:////tmp/database.db"  # Move DB to /tmp/
+    SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')  # Use environment variable
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:////tmp/database.db')  # Read DB URI from env
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = 'yashaskumar08@gmail.com'
-    MAIL_PASSWORD = 'fungfaogacdnrarm'
-    UPLOAD_FOLDER = 'new/static/media'  # Directory for media files
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')  # Read email from env
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')  # Read password from env
+
+    UPLOAD_FOLDER = 'new/static/media'
