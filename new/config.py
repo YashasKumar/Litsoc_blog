27ee4d2ea6965eb4a288e2d29f3a08cdddf07f60
YashasKumar <yashaskumar08@gmail.com>
2025-02-25 14:32:19 +0530

    vercel version changes

--- new/config.py
+++ new/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')  # Use environment variable
+    SECRET_KEY = os.getenv('SECRET_KEY', 'Hello')  # Use environment variable
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:////tmp/database.db')  # Read DB URI from env
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
